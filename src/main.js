8ad0c513e06f6a6207cecb640be46d19ae317aa2
Sandolino3 <sandolino6@gmail.com>
2022-04-12 18:24:08 +0300

    Add Selection

--- src/main.js
+++ src/main.js
@@ -1,1 +1,32 @@
-console.log(`asd`);
+let squeares = document.querySelectorAll('input')
+
+
+
+squeares.forEach(el=>{
+    el.addEventListener('click', selectRow)
+})
+
+function selectRow(e){
+let firstCross = document.getElementById('first-cross')
+let secondCross = document.getElementById('second-cross')
+let thirtCross = document.getElementById('third-cross')
+let forthCross = document.getElementById('forth-cross')
+let fiftCross = document.getElementById('fift-cross')
+    let el = e.currentTarget.parentNode
+    switch (el.id) {
+        case'second-word':
+        firstCross.classList.add('select-single')
+        fiftCross.classList.add('select-single')
+        break;
+        case'forth-word':
+        secondCross.classList.add('select-single')
+        break;
+        case'third-word':
+        thirtCross.classList.add('select-single')
+        forthCross.classList.add('select-single')
+        break;
+    
+
+    }
+    e.currentTarget.parentNode.classList.add('selected')
+}
